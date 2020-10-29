baf98974053e9f7724346f56b701fec22d77c448
Dave Woodall <hay@fakefarm.com>
2020-10-29 14:07:59 -0400

    Create Import Service
    Import is responsible for distributing extracted content and importing it into relevant Models

---
+++ spec/shared_context.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.shared_context 'shared context', shared_context: :metadata do
+  let(:upload) do
+    file = 'spec/fixtures/files/example_remit.png'
+    u = Upload.new
+    u.report.attach fixture_file_upload(file, 'image/png')
+    u.save
+    u
+  end
+
+  let(:setup) { import }
+
+  def import
+    @i ||= Import.new(upload.id).payments
+  end
+
+  let(:customer) { Customer.first }
+end
